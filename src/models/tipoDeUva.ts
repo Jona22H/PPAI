208de960ae4bbd43bff135025f380bee7b8302fc
Jona22H <jonatan.hadjadourian@gmail.com>
2024-05-22 13:55:02 -0300

    primer commit

---
+++ src/models/tipoDeUva.ts
@@ -0,0 +1,34 @@
+class TipoDeUva{
+    private _descripcion: string;
+    private _nombre: string;
+
+    constructor(nom: string, desc: string = ""){
+        this._nombre = nom;
+        this._descripcion = desc;
+    }
+
+    public getNombre(){
+        return this._nombre
+    }
+    public getDescripcion() : string{
+        return this._descripcion
+    }
+
+    public setNombre(nom : string){
+        this._nombre = nom;
+    }
+
+    public setDescripcion(desc : string){
+        this._descripcion = desc;
+    }
+}
+console.log("hola")
+let tipoMalvec = new TipoDeUva("malvec", "buena uva")
+const nom = tipoMalvec.getNombre()
+console.log(nom)
+
+
+
+tipoMalvec.setDescripcion("otra Descripcion")
+const desc = tipoMalvec.getDescripcion()
+console.log(desc)
