0cda3d4298da5ca5b9dc85edf0d0ffc62ec1266d
Jona22H <jonatan.hadjadourian@gmail.com>
2024-05-22 17:33:29 -0300

    agrego mas data

---
+++ src/models/siguiendo.ts
@@ -0,0 +1,14 @@
+export class Siguiendo{
+    private fechaInicio: Date
+    private fechaFin ?: Date
+    private bodega : Bodega
+
+    public constructor(fechaInicio : Date, bodega : Bodega, fechaFin ?: Date,  ){
+        this.fechaInicio = fechaInicio;
+        this.bodega = bodega;
+        this.fechaFin = fechaFin;
+    }
+
+}
+
+
