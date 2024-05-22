13150d4679cd0c48652869106f9e4e514cf74467
Maria Agustina Nievas Britos <89545@sistemas.frc.utn.edu.ar>
2024-05-22 20:42:07 -0300

    metodos

--- src/models/maridaje.ts
+++ src/models/maridaje.ts
@@ -1,3 +1,41 @@
+import { Vino } from "./vino";
+
 export default class Maridaje{
-  
+    private nombre: string;
+    private descripcion: string;
+    
+    constructor(nom: string, desc: string = ""){
+        this.nombre = nom;
+        this.descripcion = desc;
+    }
+    
+    public getNombre(){
+        return this.nombre
+    }
+
+    public getDescripcion(){
+        return this.descripcion
+    }
+
+    public setNombre(nom: string){
+        this.nombre = nom
+    }
+
+    public setDescripcion(desc: string){
+        return this.nombre
+    }
+
+    public sosMaridaje(tipoAComprobar: string): boolean{
+        return (this.nombre === tipoAComprobar)
+    }
+
+    public maridaConVino(vino: Vino): boolean {
+        let maridajes = vino.getMaridaje()
+        let maridajesNombres = maridajes.map(elemento => elemento.nombre)
+        return (maridajesNombres.includes(this.nombre))
+    }
+
+
 }
+
+
