a1d797de25208cceabc29e9eb0dc7636382a9f7f
gonzalo <zavalagonzalo172@gmail.com>
2024-05-25 00:50:15 -0300

    Agregamos el frontend al backend

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
+import PantallaAdministradorActualizcionBonVino from "./models/PantallaAdministradorActualizcionBonVino";
 import { Gestor } from "./models/gestor";
 
 function main(){
   const gestor = new Gestor()
+  const pantalla = new PantallaAdministradorActualizcionBonVino()
+
+  document.addEventListener('DOMContentLoaded',() => {
+    pantalla.mostrarPantalla()
+  })
 }
--- src/models/PantallaAdministradorActualizcionBonVino.ts
+++ src/models/PantallaAdministradorActualizcionBonVino.ts
@@ -7,10 +7,32 @@ export default class PantallaAdministradorActualizcionBonVino{
   public opccionImportarActualizacion(){
     this.mostrarPantalla()
   }
-  private mostrarPantalla(){
-    console.log('hola')
+  public mostrarPantalla(){
+    let pantalla = `<div>
+    <div class="background-container">
+      <div class="text-overlay">
+          <h1 id="text1">BONVINO</h1>
+          <h4 id="text2">Encuentra el vino correcto</h4>
+      </div>
+  </div>
+  </div>
+
+  <div class="container.fluid text-center" id="paddingBoton">
+    <button type="button" class="btn"  id="btIrAActualizarBodegas">Importar Actualizacion de vinos de bodega</button>
+  </div>
+
+<hr>`
+    
+    let cuerpo = document.getElementById('lol')
+    if(cuerpo){
+      cuerpo.innerHTML = pantalla
+    }
   }
   public mostrarBodegasConActu(bodeagasConActualizaciones: Bodega[]){
+
+  }
+
+  public mostrarResumenActualizacion(){
 
   }
 
