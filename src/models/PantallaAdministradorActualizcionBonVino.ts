45c5944d55568bdbf3ae7696069d19df9542f625
gonzalo <zavalagonzalo172@gmail.com>
2024-05-26 02:24:10 -0300

    Inicio de la conexion de frontend y backend

--- src/models/PantallaAdministradorActualizcionBonVino.ts
+++ src/models/PantallaAdministradorActualizcionBonVino.ts
@@ -16,43 +16,46 @@ export default class PantallaAdministradorActualizcionBonVino{
     gestor.opcionImportarActualizacion(this)
     
   }
-  public mostrarBodegasConActu(bodeagasConActualizaciones: Bodega[]){
+  public mostrarBodegasConActu(bodegasConActualizaciones: Bodega[]){
     this.cuerpo = document.getElementById('cuerpoModificable')
-    this.cuerpo.innerHTML = `<div class="checkbox-container d-flex  flex-column ms-5" id="contenido">
-    <div class="d-flex m-4">
-        <h3>Seleccionar las bodegas a actualizar</h3>
-    </div>
-    <div class="d-flex flex-wrap">
-        <fieldset id="BodegasConActualizacion">
-            
-        </fieldset>
-    </div>
-
-  </div>
-  <div class="d-flex justify-content-end  ">
-              <div class="d-flex col-3  justify-content-between ">
-                  <button class=" btn-bg-color" type="button" id="confirmarBodega">Confirmar</button>
-              </div>
-
-          </div>
-          <div>
-          </div>`
-    const fieldSet = document.getElementById('BodegasConActualizacion') 
-    
-    for(let i = 0; bodeagasConActualizaciones.length > i; i++){
-        let bodega = bodeagasConActualizaciones[i]
-        let opcion = `<div class="col-3 ">
-        <div class="form-check form-check-inline ">
-            <input class="form-check-input fs-3 " type="radio" id="inlineCheckbox${i+1}" value="${bodega.getNombre()}"
-                name="bodegaAActualizar">
-            <label class="form-check-label fs-3" for="inlineCheckbox1">${bodega.getNombre()}</label>
+    if (bodegasConActualizaciones.length > 0){
+        this.cuerpo.innerHTML = `<div class="checkbox-container d-flex  flex-column ms-5" id="contenido">
+        <div class="d-flex m-4">
+            <h3>Seleccionar las bodegas a actualizar</h3>
         </div>
-    </div>`
-        fieldSet.innerHTML += opcion
+        <div class="d-flex flex-wrap">
+            <fieldset id="BodegasConActualizacion">
+                
+            </fieldset>
+        </div>
+
+    </div>
+    <div class="d-flex justify-content-end  ">
+                <div class="d-flex col-3  justify-content-between ">
+                    <button class=" btn-bg-color" type="button" id="confirmarBodega">Confirmar</button>
+                </div>
+
+            </div>
+            <div>
+            </div>`
+        const fieldSet = document.getElementById('BodegasConActualizacion') 
         
+        for(let i = 0; bodegasConActualizaciones.length > i; i++){
+            let bodega = bodegasConActualizaciones[i]
+            let opcion = `<div class="col-3 ">
+            <div class="form-check form-check-inline ">
+                <input class="form-check-input fs-3 " type="radio" id="inlineCheckbox${i+1}" value="${bodega.getNombre()}"
+                    name="bodegaAActualizar">
+                <label class="form-check-label fs-3" for="inlineCheckbox1">${bodega.getNombre()}</label>
+            </div>
+        </div>`
+            fieldSet.innerHTML += opcion
+            
+        }
+        this.tomarSeleccionBodega(fieldSet, bodegasConActualizaciones)
+    }else{
+        //Mostrar mensaje por pantalla que no hay bodegas a actualizar y mostrar un boton que nos devuelva a la pagina anterior
     }
-    this.tomarSeleccionBodega(fieldSet, bodeagasConActualizaciones)
-    
 
   }
 
@@ -60,7 +63,7 @@ export default class PantallaAdministradorActualizcionBonVino{
 
   }
 
-  public tomarSeleccionBodega(fieldSet: HTMLElement | null, bodeagasConActualizaciones: Bodega[]){
+  public tomarSeleccionBodega(fieldSet: HTMLElement | null, bodegasConActualizaciones: Bodega[]){
     const boton = document.getElementById('confirmarBodega')
     boton.addEventListener('click', () => {
         const radios = fieldSet.querySelectorAll('input[type="radio"]')
@@ -73,7 +76,7 @@ export default class PantallaAdministradorActualizcionBonVino{
         })
 
         if (bodegaSeleccionada){
-            let bodegaAEnviar = bodeagasConActualizaciones.find((bodega: Bodega) => bodega.getNombre().toLowerCase() === bodegaSeleccionada.toLowerCase()) 
+            let bodegaAEnviar = bodegasConActualizaciones.find((bodega: Bodega) => bodega.getNombre().toLowerCase() === bodegaSeleccionada.toLowerCase()) 
             this.gestor.tomarSeleccionBodega(bodegaAEnviar)
         }else{
             console.log('No ando')
