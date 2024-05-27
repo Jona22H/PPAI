36420a17fe4f4f9cff6c1bfe186ebf5c7f72e90a
gonzalo <zavalagonzalo172@gmail.com>
2024-05-27 19:55:01 -0300

    PRIMERA ENTREGA TERMINADA

--- src/models/gestor.ts
+++ src/models/gestor.ts
@@ -70,7 +70,7 @@ export default class Gestor {
       this.actualizarVinosDeBodega()
       //console.log(this.vinosAMostrar[0].vinoAMostrar)
     } else {
-      bodegaAActualizar.setFechaUltimaActualizacion(new Date())
+      //bodegaAActualizar.setFechaUltimaActualizacion(new Date())
       this.pantalla.mostrarPantallaError()
       //En vez de mostrar la tabla vamos a mostrar un mensaje de error respecto a la no respuesta de la API y el boton dira Volver al menú principal
     }
